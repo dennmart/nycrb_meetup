aba2f800aaca6c83378eb6c7ac9666a9c3b994ce
Dennis Martinez <dennis@dennmart.com>
2024-08-26 14:34:10 +0900

    [WIP] Create message from form submission

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,33 @@
 class MessagesController < ApplicationController
+  before_action :set_uuid
+
   def index
+    @messages = Message.order(created_at: :desc).limit(10)
   end
 
   def create
-    if params[:comment].blank?
-      head :bad_request
-    else
+    @message = Message.new(message_params)
+
+    if @message.save
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.prepend("messages", partial: "messages/message", locals: { message: params[:comment] }) }
+        format.turbo_stream { render turbo_stream: turbo_stream.prepend("messages", partial: "messages/message", locals: { message: @message }), status: :created }
         format.html { redirect_to root_path }
       end
+    else
+      head :bad_request
     end
   end
 
   def all
+  end
+
+  private
+
+  def set_uuid
+    session[:uuid] = SecureRandom.uuid if session[:uuid].blank?
+  end
+
+  def message_params
+    params.require(:message).permit(:uuid, :content)
   end
 end
--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,7 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe "Messages", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /" do
+    it "fetches the last 10 messages in descending order of created_at" do
+      messages_double = double("ActiveRecord::Relation")
+      message = Message.create!(uuid: SecureRandom.uuid, content: "Hello, World!")
+
+      expect(Message).to receive(:order).with(created_at: :desc).and_return(messages_double)
+      expect(messages_double).to receive(:limit).with(10).and_return(message)
+      get messages_path
+    end
+
+    it "returns a successful response" do
+      get messages_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /messages" do
+    let(:uuid) { SecureRandom.uuid }
+
+    context "unsuccessful creation" do
+      it "does not create a new Message" do
+        expect {
+          post messages_path, params: { message: { uuid: uuid, content: "" } }
+        }.not_to change(Message, :count)
+      end
+
+      it "returns a 400 Bad Request response" do
+        post messages_path, params: { message: { uuid: uuid, content: "" } }
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context "successful creation" do
+      it "creates a new Message" do
+        expect {
+          post messages_path, params: { message: { uuid: uuid, content: "Hello, World!" } }
+        }.to change(Message, :count).by(1)
+
+        message = Message.last
+        expect(message.uuid).to eq(uuid)
+        expect(message.content).to eq("Hello, World!")
+      end
+
+      it "returns a 201 Created response for Turbo Stream" do
+        post messages_path, params: { message: { uuid: uuid, content: "Hello, World!" } }, as: :turbo_stream
+        expect(response).to have_http_status(:created)
+      end
+
+      it "redirects to the root path when not Turbo Stream" do
+        post messages_path, params: { message: { uuid: uuid, content: "Hello, World!" } }
+        expect(response).to be_redirect
+        expect(response).to redirect_to(root_path)
+      end
+    end
   end
 end
